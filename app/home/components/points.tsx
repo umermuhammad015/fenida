f3df992be2ab9e9c15ff2dc81b22d4057e64a82a
umermuhammad015 <153215851+umermuhammad015@users.noreply.github.com>
2025-02-17 21:19:29 +0500

    u

--- app/home/components/points.tsx
+++ app/home/components/points.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import { useEffect, useState } from "react";
 import { useSearchParams } from 'next/navigation'
 import {
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 // import Image from 'next/image'
-import UpcomingMatches from './home/components/upcoming_matches'
+// import UpcomingMatches from './home/components/upcoming_matches'
 
 
 export default function Home() {
   return (
 
-      <main className="container justify-between p-24">
+    <main className="container justify-between p-24">
+      hi
+      {/* <UpcomingMatches /> */}
 
-        <UpcomingMatches />
 
-
-      </main>
-      )
+    </main>
+  )
 }
