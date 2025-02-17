5ba0d1e3c3b0e90e4d783219eb074da5b26228e5
umermuhammad015 <153215851+umermuhammad015@users.noreply.github.com>
2025-02-17 21:34:00 +0500

    u

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -10,112 +10,13 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ [
 
 
     const {
-        league= "ENG1",
+        // league= "ENG1",
         season = "2024",
-        // team = "Nottingham Forest",
         user_team = "Nottingham Forest",
         user_league_code = "ENG1"
 
     } = await searchParams;
 
-
-
-    // await new Promise(resolve => setTimeout(resolve, 3000));
-
-    // const user_team = "Nottingham Forest";
-    // const user_league_code = "ENG1";
-
-    // Query to fetch data from database table
-    // const upcoming_matches = await prisma.upcoming_matches.findMany({
-    //     select: {
-    //         home_team: true,
-    //         away_team: true,
-    //         home_team_short: true,
-    //         away_team_short: true,
-    //         league: true,
-    //         date: true,
-    //         prob1: true,
-    //         prob_draw: true,
-    //         prob2: true,
-    //         id: true,
-    //         country: true,
-
-    //     },
-    //     where: {
-    //         OR: [{ home_team: user_team }, { away_team: user_team }],
-
-    //         league_code: user_league_code,
-
-    //     },
-
-    // });
-
-
-    // Query to fetch data from database table
-    // const dashboard_points = await prisma.dashboard_points.findFirst({
-    //     select: {
-
-    //         total_home_deserved_points: true,
-    //         total_home_predicted_points: true,
-    //         total_home_points: true,
-    //         total_home_deserved_points_percentile: true,
-    //         total_home_points_percentile: true,
-    //         total_home_predicted_points_percentile: true,
-    //         total_away_deserved_points: true,
-    //         total_away_predicted_points: true,
-    //         total_away_points: true,
-    //         total_away_deserved_points_percentile: true,
-    //         total_away_points_percentile: true,
-    //         total_away_predicted_points_percentile: true,
-    //         total_points: true
-
-    //     },
-    //     where: {
-    //         team: user_team
-    //     },
-    // });
-
-    // const completed_matches = await fetchCompletedMatches(user_league_code, user_team);
-
-    // console.log("completed_matches from parent")
-    // // console.log(completed_matches)
-
-
-
-    // const league_table_old = await prisma.eng1_forecast_wide.findMany({
-    //     select: {
-    //         team: true,
-    //         place1: true,
-    //         place2: true,
-    //         place3: true,
-    //         place4: true,
-    //         place5: true,
-    //         place6: true,
-    //         place7: true,
-    //         place8: true,
-    //         place9: true,
-    //         place10: true,
-    //         place11: true,
-    //         place12: true,
-    //         place13: true,
-    //         place14: true,
-    //         place15: true,
-    //         place16: true,
-    //         place17: true,
-    //         place18: true,
-    //         place19: true,
-    //         place20: true
-    //     },
-    //     where: {
-
-    //     },
-    // });
-
-    // console.log(upcoming_matches)
-
-    // console.log(league_table)
-
-    // console.log("page loaded")
 
 
 
